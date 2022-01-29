3db64581e79543912cfebc4e2b21921fe56c50f4
Robert Xu <Robert_xu2010@hotmail.com>
2022-01-29 12:46:01 +1030

    add comments for tests

--- boggle_test.py
+++ boggle_test.py
@@ -13,6 +13,7 @@ class BoggleTest(unittest.TestCase):
     # Test suite for boggle
     
     def test_read_words(self):
+        # Test that read_words returns sets that have lengths greater than 0
         dictionary, prefixes = boggle.read_words("dictionary.txt")
         self.assertGreater(len(dictionary), 0)
         self.assertGreater(len(prefixes), 0)
@@ -28,6 +29,7 @@ class BoggleTest(unittest.TestCase):
     
     
     def test_all_neighbours(self):
+        # Test set_all_neighbours() on a small 3x3 board
         for i in range(len(TEST_BOARD)):
             self.assertNotIn(i, boggle.all_neighbours[i])
             self.assertNotIn(-1, boggle.all_neighbours[i])
@@ -44,16 +46,21 @@ class BoggleTest(unittest.TestCase):
 
         
     def test_boggle_contain(self):
+        # Ensure that all possible words are present in the Boggle solution
         solutions = boggle.solve_boggle(TEST_BOARD)
         possible_words = {"DUO", "TOD", "OUT", "LOO", "LOT", "OUD", "LOB",
-                          "BOOT", "LOOT", "FOOT", "TOFU", "FOOL", "BLOT"}
+                          "BOOT", "BOUT", "FOOT", "TOFU", "FOOL", "BOLO",
+                          "LOOT", "TOOL", "LOUD", "BLOT", "FLOUT", "BLOOD"}
         for word in possible_words:
             self.assertIn(word, solutions)
-    
+
     
     def test_boggle_not_contain(self):
+        # Ensure that all of the impossible words are not in the Boggle solution
         solutions = boggle.solve_boggle(TEST_BOARD)
-        impossible_words = {"BOLD", "DOLL", "FOOD", "TOLD", "BOLT", "TOOT"}
+        impossible_words = {"OF", "TO", "BO", "DO", "AT", "IT", "BY", "DY",
+                            "ODD", "TOT", "BOB", "DUD", "LUD", "BAT", "BUD"
+                            "BOLD", "DOLL", "FOOD", "TOLD", "BOLT", "TOOT"}
         for word in impossible_words:
             self.assertNotIn(word, solutions)
 
